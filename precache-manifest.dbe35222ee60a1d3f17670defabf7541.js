ae11cb99b8d57c522068e615d72e61abec620181
alexislagarde <42228717+alexislagarde@users.noreply.github.com>
2020-03-08 15:53:12 -0300

    Updates

---
+++ precache-manifest.dbe35222ee60a1d3f17670defabf7541.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b700ae81ca06b0530adc59e15c5bee50",
+    "url": "/vica/index.html"
+  },
+  {
+    "revision": "3d46c1d24f134ee4b7ae",
+    "url": "/vica/static/css/main.f4a50cff.chunk.css"
+  },
+  {
+    "revision": "e1d47f77f46cedda7da6",
+    "url": "/vica/static/js/2.68d35397.chunk.js"
+  },
+  {
+    "revision": "1aff40f6cd2589c1f06af7e73a2046f9",
+    "url": "/vica/static/js/2.68d35397.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d46c1d24f134ee4b7ae",
+    "url": "/vica/static/js/main.b1ff605c.chunk.js"
+  },
+  {
+    "revision": "930e10302b02243321c0",
+    "url": "/vica/static/js/runtime-main.a1aac4f4.js"
+  },
+  {
+    "revision": "a0fc7c236af0d7bbe91283e9bae28705",
+    "url": "/vica/static/media/LogoHeaderppal.a0fc7c23.jpg"
+  },
+  {
+    "revision": "3a24dd155831940d28c971c026ee71e4",
+    "url": "/vica/static/media/LogoPie.3a24dd15.jpg"
+  },
+  {
+    "revision": "ca967c2204603da94e126f61d5f95fbb",
+    "url": "/vica/static/media/login_vica.ca967c22.jpg"
+  },
+  {
+    "revision": "d4fee6855e0c5eb88eb4e293356e55a9",
+    "url": "/vica/static/media/vica-logo.d4fee685.png"
+  },
+  {
+    "revision": "393cf51f75ed168984713e9a6f610b96",
+    "url": "/vica/static/media/vicaneum2.393cf51f.jpg"
+  }
+]);
